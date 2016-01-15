83980f5755bd1cf7c755fc1b906e169a068deeb3
swiftyy188 <jeffho.ml@gmail.com>
2016-01-15 17:17:01 -0500

    Creating log in functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+	def new
+		
+	end
+
+	def create
+		user = User.find_by(email: params[:email])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.user_id
+			redirect_to products_url, notice: "logged in!"
+		else
+			render "new"
+		end
+	end
+	def destory
+		session[:user_id] = nil
+		redirect_to products_url, notice: "Logged out!"		
+	end
+		
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root'projects#index'
   resources :projects
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
